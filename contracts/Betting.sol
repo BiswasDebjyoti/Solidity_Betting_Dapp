181dba43af48549a73ce5d4900195be7c35eae14
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-10-30 16:25:12 +0530

    Added functions and state variables to contract-Betting

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -1,7 +1,18 @@
 pragma solidity ^0.4.4;
 
 contract Betting {
-  function Betting() {
+  uint[3] params=[0,0,0];
+  function Betting(uint8 param1,uint8 param2,uint8 param3) {
     // constructor
-  }
+    
+    	params[0]=param1;
+    	params[1]=param2;
+    	params[2]=param3;
+    }
+
+
+    function guess(uint8 guess) returns(bool)
+    {
+    	return true;
+    }
 }
