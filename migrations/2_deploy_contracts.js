784e19b909e04e043669acf9e099628cc8c06c2a
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-10-31 09:24:13 +0530

    Added new files --working better --not able to save state yet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Betting = artifacts.require("./Betting.sol");
+var MultiNumberBettingV1 = artifacts.require("./MultiNumberBettingV1.sol"); 
+
 
 module.exports = function(deployer) {
   deployer.deploy(Betting,2,3,6);
+  deployer.deploy(MultiNumberBettingV1,1,3,9);
 };
