474885bfb7b459977503c31ba103fc185bbeba47
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-11-03 13:36:47 +0530

    guess funtion using person struct

---
+++ contracts/Betting1.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.4;
+
+contract Betting {
+  uint[3] params=[0,0,0];
+  uint8  winnerCount=0;
+  uint8  loserCount=0;
+  uint lastWinnerAt;
+  string lastWinnerName="***";
+  uint guessedAt = now;
+  //uint8  guess;
+  address winner_Address;
+  
+//Winner Struct
+  struct person
+  {
+    uint guess_;
+  }
+
+  person person1;
+  
+
+
+
+    // constructor
+  function Betting(uint8 param1,uint8 param2,uint8 param3) {
+    
+    
+    	params[0]=param1;
+    	params[1]=param2;
+    	params[2]=param3;
+    }
+    // returns loser count
+    function returnLoser() returns(uint)
+    {
+    	return loserCount;
+    }
+    //returns loser count
+    function returnWinner() returns(uint)
+    {
+    	return winnerCount;
+    }
+    //returns last winners name
+    function getLastWinner() returns(string)
+    {
+        return lastWinnerName;
+    }
+
+    // guess function
+	
+	function guess( string name)
+    {	uint i=0;
+      uint guess=person1.guess;
+    //for loop to check if guess is there
+        if(guess>10) revert();
+    	for(i=0;i<3;i++)
+    	{
+    		if(params[i]==guess)
+    		{
+    			//return true;
+                winnerCount++;
+                lastWinnerName=name;
+    			lastWinnerAt=now;
+                winner_Address=msg.sender;
+                break;
+    		}
+    	}
+        if(i==3)
+        {
+        loserCount++;
+        }
+  	 
+    	
+    }
+    //getter time
+    function LastWinnerMinute() returns(uint)
+    {
+        return (now - lastWinnerAt*1 minutes);
+    }
+    //WInner Address
+    function winnerAddress() returns(address)
+    {
+        return winner_Address;
+    }
+    // guess setter
+    function guessSet(uint guessSet)
+    {
+      person1.guess_=guessSet;
+    }
+    //Name Setter
+}
